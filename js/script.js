ec55bb70c5336486db247142240013bf9b142a17
Tõnu <tonu.trubetsky@codelab.ee>
2019-12-03 10:48:29 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.danger').addEventListener('click', function(){
+  document.getElementById('pop-up').style.display = 'block';
+})
+
+document.querySelector('.close').addEventListener('click', function(){
+  document.getElementById('pop-up').style.display = 'none';
+})
